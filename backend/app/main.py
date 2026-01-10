ce0a7094bf75a5b7c388615efe1f75d81c24cf52
Dipak Kumar Chauhan <ece22133@iiitkalyani.ac.in>
2026-01-10 19:33:10 +0530

    CORS Modified

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,14 +18,14 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-
-        # ✅ ADDED Vercel frontend
         "https://primetradeai.vercel.app",
     ],
+    allow_origin_regex=r"https://primetradeai-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 
