e82c15d20f3f2af93d2c2b5487521c67dee403f4
Dipak Kumar Chauhan <ece22133@iiitkalyani.ac.in>
2026-01-10 19:09:45 +0530

    Vercel url added in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,11 +18,15 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+
+        # ✅ ADDED Vercel frontend
+        "https://primetradeai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 app.include_router(auth_router, prefix="/api/v1")
